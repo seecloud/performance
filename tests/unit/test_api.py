8fd67e6f5d5fbc2c9683cf5fec9cbb2a72685d77
Alexander Maretskiy <amaretskiy@mirantis.com>
2016-11-15 15:34:56 +0200

    Add api map and api module for future needs
    performance.api module will be used for RESTful API implementation.
    API map is added using flask-helpers library.

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -13,33 +13,17 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import flask
-from flask_helpers import routing  # noqa
+from tests.unit import test  # noqa
 
 from performance import api
-from performance import config
-
-app = flask.Flask(__name__, static_folder=None)
-app.config.update(config.get_config()["flask"])
 
 
-for module in [api]:
-    for url_prefix, blueprint in module.get_blueprints():
-        app.register_blueprint(blueprint, url_prefix="/api%s" % url_prefix)
+class APITestCase(test.TestCase):
 
-
-app = routing.add_routing_map(app, html_uri=None, json_uri="/")
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return flask.jsonify({"error": "Not Found"}), 404
-
-
-def main():
-    app.run(host=app.config.get("HOST", "0.0.0.0"),
-            port=app.config.get("PORT", 5010))
-
-
-if __name__ == "__main__":
-    main()
+    def test_get_blueprints(self):
+        result = api.get_blueprints()
+        bp = result[0][1]
+        result[0][1] = "foo_bp"
+        self.assertEqual([["/dummy", "foo_bp"]], result)
+        self.assertIsInstance(bp, api.flask.Blueprint)
+        self.assertEqual("dummy", bp.name)
